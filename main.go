ddaac99a53668f9a07bc50255cc2f9f23516dcb6
naufal360 <naufal360.an@gmail.com>
2023-04-21 21:51:13 +0700

    initial project

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"go-final-project-mygram/app"
+	"go-final-project-mygram/config"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error .env file at %s", err)
+		panic(err)
+	}
+
+	err = config.InitGorm()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	app.StartApp()
+}
