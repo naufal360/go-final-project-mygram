f7b855d9de9783ebb051227101aafdccdb06a7bd
naufal360 <naufal360.an@gmail.com>
2023-04-22 18:52:54 +0700

    new config os

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	github.com/google/uuid v1.3.0
-	github.com/joho/godotenv v1.5.1
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.8.12
--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"go-final-project-mygram/app"
 	"go-final-project-mygram/config"
-	"log"
-
-	"github.com/joho/godotenv"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("error .env file at %s", err)
-		panic(err)
-	}
+	// debug env
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("error .env file at %s", err)
+	// 	panic(err)
+	// }
 
-	err = config.InitGorm()
+	err := config.InitGorm()
 	if err != nil {
 		panic(err)
 	}
